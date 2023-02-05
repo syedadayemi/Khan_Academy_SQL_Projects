1feece269e02c0e0a10c6eb62c1b78f547124a75
syedadayemi <119980176+syedadayemi@users.noreply.github.com>
2023-02-05 10:46:21 -0600

    Create More_advanced_SQL_queries.sql

---
+++ More_advanced_SQL_queries.sql
@@ -0,0 +1,6 @@
+/*
+Ever sung karaoke? It's a place where you sing songs with your friends, and it's a lot of fun. 
+We've created a table with songs, and in this challenge, you'll use queries to decide what songs to sing. 
+For the first step, select all the song titles.
+*/
+
